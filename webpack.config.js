c77cdf553e1e82de1f429341cf85e62a73658a65
D. Wren <drawnwren@gmail.com>
2016-10-10 18:47:09 -0700

    Added localhost redirect option.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const BUILD_DIR = path.resolve(__dirname, 'client/public');
 const APP_DIR = path.resolve(__dirname, 'client');
+// Redirect to localhost if the REDIRECT variable is set
+const REDIRECT = process.env.REDIRECT ? 'http://localhost:9009' : 'http://52.52.22.4';
 
 const config = {
   entry: './app/routes.jsx',
@@ -14,11 +16,11 @@ const config = {
   devServer: {
     proxy: {
       '/api/**': {
-        target: 'http://52.52.22.4',
+        target: REDIRECT, 
         secure: false
       },
       '/auth/**': {
-        target: 'http://52.52.22.4',
+        target: REDIRECT,
         secure: false
       }
     },
